b9cf0f0b74637a411ecc54066784201a20bb93dd
Scooterlicious <sfell2006@gmail.com>
2014-03-23 12:31:52 -0400

    Added scooter and a few tools icons plus work on preloading of heroku apps

--- scripts/preload.js
+++ scripts/preload.js
@@ -1,2 +1,50 @@
-$.get("http://cheap-new-york.herokuapp.com/");
-$ajax("http://cherry-timely.herokuapp.com/");
+$(document).ready(function(){	//executed after the page has loaded
+
+	$.ajax({	//create an ajax request to heroku apps
+        type: "GET",
+        url: "http://cherry-timely.herokuapp.com",
+        data: '',
+        dataType: "html",	//expect html to be returned
+        success: function(msg){
+        	console.log("SUCCESS: Returned from cherry-timely!");
+        	console.log(msg);
+        },
+        error:  function(msg){
+        	console.log("ERROR: Returned from cherry-timely!");
+        	console.log(msg);
+        }
+    });
+
+	$.ajax({	//create an ajax request to heroku apps
+        type: "GET",
+        url: "http://knowthyselfie.herokuapp.com",
+        data: '',
+        dataType: "html",	//expect html to be returned
+        success: function(msg){
+        	console.log("SUCCESS: Returned from KTS!");
+        	console.log(msg);
+        },
+        error:  function(msg){
+        	console.log("ERROR: Returned from KTS!");
+        	console.log(msg);
+        }
+    });
+
+	$.ajax({	//create an ajax request to heroku apps
+        type: "GET",
+        url: "http://cheap-new-york.herokuapp.com/",
+        data: '',
+        dataType: "html",	//expect html to be returned
+        success: function(msg){
+        	console.log("SUCCESS: Returned from CNY!");
+        	console.log(msg);
+        },
+        error:  function(msg){
+        	console.log("ERROR: Returned from CNY!");
+        	console.log(msg);
+        }
+    });
+
+});
+
+
